a6a9f791ed8c8838784818bb812a19330271f607
ReeceT3a <271935668+ReeceT3a@users.noreply.github.com>
2026-08-01 10:31:14 +0100

    Fix mobile arrival slider

---
+++ assets/customer-BJNjEKre.js
@@ -0,0 +1,1 @@
+import{c as e,j as t,r as o}from"./client-BnsUgxtu.js";import{H as r}from"./customer-PRwB-vpD.js";e.createRoot(document.getElementById("root")).render(t.jsx(o.StrictMode,{children:t.jsx(r,{initialRole:"customer"})}));
